76507be3795669f1e5c9ec2c0611d293a7e2a99a
Egezenn <egezenn@tutanota.com>
2026-04-25 17:45:48 +0300

    RE

--- Minify/core/steam.py
+++ Minify/core/steam.py
@@ -107,9 +107,9 @@ def fix_launch_options():
 
             terminal.add_text("&discrepancy_launch_options", user_name, locale)
 
-            data["UserLocalConfigStore"]["Software"]["Valve"]["Steam"]["apps"][base.STEAM_DOTA_ID][
-                "LaunchOptions"
-            ] = f"-language {locale} {remove_lang_args(launch_options)}"
+            data["UserLocalConfigStore"]["Software"]["Valve"]["Steam"]["apps"][base.STEAM_DOTA_ID]["LaunchOptions"] = (
+                f"-language {locale} {remove_lang_args(launch_options)}"
+            )
         with utils.open_utf8R(vdf_path, "w") as file:
             vdf.dump(data, file, pretty=True)
         successful_ids.append(steam_id)
